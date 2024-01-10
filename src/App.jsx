f401f4b71fd6e4dd5f8b4379a9549feacd418a32
diase <eduardodias830@gmail.com>
2024-01-09 23:09:38 -0300

    :tada: Primeiro commit.

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const App = _ => <div>Olá React</div>;
+
+export default App;
